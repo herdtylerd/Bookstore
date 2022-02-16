85a86b2991045fff0fba40aecc187e8b67e42d34
herdtylerd <91562573+herdtylerd@users.noreply.github.com>
2022-02-16 15:04:23 -0700

    Bootstrap for Mission 7

--- Bookstore/Infrastructure/BookstorePagination.cs
+++ Bookstore/Infrastructure/BookstorePagination.cs
@@ -29,17 +29,33 @@ namespace Bookstore.Infrastructure
         public PageInfo PageNums { get; set; }
         public string PageAction { get; set; }
 
+        // Bootstrap
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; } 
+
+
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
 
             TagBuilder final = new TagBuilder("div");
 
-            for (int i = 1; i < PageNums.TotalPages; i++)
+            for (int i = 1; i <= PageNums.TotalPages; i++)
             {
                 TagBuilder tb = new TagBuilder("a");
 
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageNums.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
+
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
