90ee1d23522ca79ba5719c874283c4ab8e752db3
herdtylerd <91562573+herdtylerd@users.noreply.github.com>
2022-02-23 12:25:48 -0700

    Shopping cart feature, Sessions, Razor Pages

---
+++ Bookstore/Models/Cart.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public class Cart
+    {
+        public List<CartLineItem> Items { get; set; } = new List<CartLineItem>();
+
+        public void AddItem (Book book, int qty)
+        {
+            CartLineItem line = Items
+                .Where(b => b.Book.BookId == book.BookId)
+                .FirstOrDefault();
+
+            if (line == null) // Create new item in cart if already doesn't exist
+            {
+                Items.Add(new CartLineItem
+                {
+                    Book = book,
+                    Quantity = qty
+                });
+            }
+            else
+            {
+                line.Quantity += qty; // Add qty to existing item in cart
+            }
+        }
+
+        public double CalculateTotal()
+        {
+            double sum = Items.Sum(x => x.Quantity * x.Book.Price);
+            return sum;
+        }
+    }
+
+    public class CartLineItem
+    {
+        public int LineID { get; set; }
+        public Book Book { get; set; }
+        public int Quantity { get; set; }
+    }
+}
